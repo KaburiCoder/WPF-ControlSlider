5529647d76fd875564ea8922a6878e2f95449496
KaburiCorder <specialemail90@gmail.com>
2023-01-08 23:23:38 +0900

    ControlSlider 강의 소스코드 2편

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,6 +1,5 @@
 ﻿using System.Windows;
 using WpfControlSlider.ViewModels;
-using WpfLib.ViewModels;
 
 namespace WpfControlSlider
 {
@@ -9,52 +8,10 @@ namespace WpfControlSlider
   /// </summary>
   public partial class MainWindow : Window
   {
-    private ViewModelBase _currentViewModel = default!;
-
-    public ViewModelBase CurrentViewModel
-    {
-      get
-      {
-        if (_currentViewModel is FirstViewModel)
-        {
-          _currentViewModel = new SecondViewModel();
-        }
-        else
-        {
-          _currentViewModel = new FirstViewModel();
-        }
-        return _currentViewModel;
-      }
-    }
-
-
     public MainWindow()
     {
       InitializeComponent();
-    }
-
-    private void LeftToRight_Clicked(object sender, RoutedEventArgs e)
-    {
-      slider.SetAnimationSpeed(100);
-      slider.Slide(CurrentViewModel, WpfLib.Controls.SlideType.LeftToRight);
-    }
-
-    private void RightToLeft_Clicked(object sender, RoutedEventArgs e)
-    {
-      slider.SetAnimationSpeed(300);
-      slider.Slide(CurrentViewModel, WpfLib.Controls.SlideType.RightToLeft);
-    }
-
-    private void TopToBottom_Clicked(object sender, RoutedEventArgs e)
-    {
-      slider.SetAnimationSpeed(700);
-      slider.Slide(CurrentViewModel, WpfLib.Controls.SlideType.TopToBottom);
-    }
-
-    private void BottomToTop_Clicked(object sender, RoutedEventArgs e)
-    {
-      slider.SetAnimationSpeed(1000);
-      slider.Slide(CurrentViewModel, WpfLib.Controls.SlideType.BottomToTop);
+      DataContext = new MainViewModel();
     }
   }
 }
---
+++ ViewModels/MainViewModel.cs
@@ -0,0 +1,92 @@
+﻿using System.Windows.Input;
+using WpfLib.Commands;
+using WpfLib.Controls;
+using WpfLib.ViewModels;
+
+namespace WpfControlSlider.ViewModels
+{
+  public class MainViewModel : ViewModelBase
+  {
+    private ViewModelBase _currentViewModel = default!;
+    private SlideType _slideType;
+
+    private void BottomToTop(object _)
+    {
+      SlideType = SlideType.BottomToTop;
+      CurrentViewModel = GetViewModel();
+    }
+
+    private void TopToBottom(object _)
+    {
+      SlideType = SlideType.TopToBottom;
+      CurrentViewModel = GetViewModel();
+    }
+
+    private void RightToLeft(object _)
+    {
+      SlideType = SlideType.RightToLeft;
+      CurrentViewModel = GetViewModel();
+    }
+
+    private void LeftToRight(object _)
+    {
+      SlideType = SlideType.LeftToRight;
+      CurrentViewModel = GetViewModel();
+    }
+
+    private ViewModelBase GetViewModel()
+    {
+      if (_currentViewModel is FirstViewModel)
+      {
+        return new SecondViewModel();
+      }
+      else
+      {
+        return new FirstViewModel();
+      }
+    }
+
+    public MainViewModel()
+    {
+      CurrentViewModel = new FirstViewModel();
+      LeftToRightCommand = new RelayCommand<object>(LeftToRight);
+      RightToLeftCommand = new RelayCommand<object>(RightToLeft);
+      TopToBottomCommand = new RelayCommand<object>(TopToBottom);
+      BottomToTopCommand = new RelayCommand<object>(BottomToTop);
+    }
+
+    public ViewModelBase CurrentViewModel
+    {
+      get
+      {
+        return _currentViewModel;
+      }
+      set
+      {
+        if (_currentViewModel != value)
+        {
+          _currentViewModel = value;
+          OnPropertyChanged();
+        }
+      }
+    }
+
+    public SlideType SlideType
+    {
+      get { return _slideType; }
+      set
+      {
+        if (_slideType != value)
+        {
+          _slideType = value;
+          OnPropertyChanged();
+        }
+      }
+    }
+
+    public ICommand LeftToRightCommand { get; set; }
+    public ICommand RightToLeftCommand { get; set; }
+    public ICommand TopToBottomCommand { get; set; }
+    public ICommand BottomToTopCommand { get; set; }
+  }
+}
